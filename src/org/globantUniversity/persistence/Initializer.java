0f2e765936ee2bf6d260ef9926e59245df86d24f
Daniel Correa <91207654+Daniel3132@users.noreply.github.com>
2022-10-26 09:41:20 -0500

    add initializer class

---
+++ src/org/globantUniversity/persistence/Initializer.java
@@ -0,0 +1,26 @@
+package org.globantUniversity.persistence;
+
+import org.globantUniversity.data.University;
+
+public class Initializer {
+
+    public static University createUniversity() {
+        University university = new University();
+
+        return university;
+    }
+
+    public static void createTeachers() {
+        
+    }
+    
+    public static void createStudents() {
+        
+    }
+    
+    public static void createSubjects() {
+        
+    }
+
+    
+}
